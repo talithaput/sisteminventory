605f44d26593c9e268811bd6357809aee999bf15
Putri Talitha Wardani <94584959+talithaput@users.noreply.github.com>
2023-01-24 16:56:36 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -8,6 +8,4 @@ $db_name = "inventory";
 
 $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-$join           = "select * from alat_tulis join user on user.id = alat_tulis.id_user";
-$select         = mysqli_query($connection, $join);
 ?>
